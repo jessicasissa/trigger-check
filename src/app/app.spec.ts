4541a8b7812c4eb2aefbce01f83d3b1dca97fa52
jessicasissa <jeloytza@gmail.com>
2026-09-13 00:05:30 -0300

    feat(app name): atualiza o nome do app pra `TriggerCheck`

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, mtg-project');
+    expect(compiled.querySelector('h2')?.textContent).toContain('TriggerCheck');
   });
 });
