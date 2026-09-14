3c87dc9e1189bdb70729398e17455d328ee5e6e6
jessicasissa <jeloytza@gmail.com>
2026-09-14 01:00:48 -0300

    refactor: ajusta nomenclatura do `MtgService` e remove import inutilizado do `app.ts`

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { CardList } from "./components/card-list/card-list";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CardList],
+  imports: [CardList],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
